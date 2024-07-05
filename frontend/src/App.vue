7f31fd8daa1d4c49f1ec65ca121a8fd179b5e1a0
Leesohyun <vir5312@naver.com>
2024-07-05 16:57:13 +0900

    FEAT:frontend 프로젝트 추가

---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-main>
+      <IndexMap/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import IndexMap from './components/IndexMap.vue'
+
+
+export default {
+  name: 'App',
+
+  components: {
+    IndexMap,
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
---
+++ frontend/src/components/IndexMap.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="map" style="width:100%; height:800px;"></div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    // 네이버 지도 API 로드
+    const script = document.createElement("script");
+    script.src = "https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=" + process.env.VUE_APP_MAPURL
+    script.async = true;
+    script.defer = true;
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      // 네이버 지도 생성
+      new window.naver.maps.Map("map", {
+        center: new window.naver.maps.LatLng(37.5670135, 126.9783740),
+        zoom: 10
+      });
+    };
+  }
+};
+</script>
+  
