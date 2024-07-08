c158f88d773e375f5341bd0013733a9e49c3362d
kimgibok <ruddjs33@naver.com>
2024-07-08 10:59:54 +0900

    Fix : SearchHistory모델수정(place, created_at)

--- backend/history/models.py
+++ backend/history/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.conf import settings
-from search.models import Address
+from search.models import Place
 
-# Create your models here.
-
+# Create your models here.   
 class SearchHistory(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    address = models.ForeignKey(Address, on_delete=models.DO_NOTHING)
+    place = models.ForeignKey(Place, null=True, blank=True, on_delete=models.SET_NULL)
     search = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.search}"
