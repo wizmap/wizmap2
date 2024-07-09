1ff021233c63f90fafcae82fd60d0b6b91294a31
Mansik_kun <wjdalstjr730@gmail.com>
2024-07-09 10:47:30 +0900

    Feat:Add Quick
    즐겨찾기 뷰  추가,퀵기능 추가

--- backend/favorites/serializers.py
+++ backend/favorites/serializers.py
@@ -1,12 +1,17 @@
 from rest_framework import serializers
-from .models import List, MyPin
+from .models import List, MyPin,QuickSlot
 
 class ListSerializer(serializers.ModelSerializer):
     class Meta:
         model = List
-        fields = ['id', 'name', 'user']
+        fields = ['id', 'name', 'user',]
 
 class MyPinSerializer(serializers.ModelSerializer):
     class Meta:
         model = MyPin
         fields = ['id', 'list', 'place', 'name']
+
+class QuickSlotSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = QuickSlot
+        fields = ['id', 'name', 'user','place','type']
