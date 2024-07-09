7968b9d83d613312b56c14a7f327cad9f2ce9562
Hyeonboong <krystal6345@naver.com>
2024-07-09 11:24:55 +0900

    test

--- backend/wizmap/settings.py
+++ backend/wizmap/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     
     #app
     'user',
@@ -45,6 +46,11 @@ INSTALLED_APPS = [
     'history',
     'favorites',
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
