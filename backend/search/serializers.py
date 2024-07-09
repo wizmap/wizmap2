8d6d77908ad93be6770ba10a33ebbf2efbadd0cb
kmj <kimmj7916@gmail.com>
2024-07-09 10:52:50 +0900

    FEAT:현재 영업 여부 확인 기능 추가

--- backend/search/serializers.py
+++ backend/search/serializers.py
@@ -18,7 +18,6 @@ class PlaceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Place
         fields = ['address', 'name', 'menu', 'phone', 'memo', 'category', 'is_open', 'business_hours']
-        # is_open 필드를 category 다음에 정의
 
     def get_is_open(self, obj):
         return CheckBusinessHour(obj)
