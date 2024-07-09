6bfb0720f12d6a1e72556893f50199245df947a8
Hyeonboong <krystal6345@naver.com>
2024-07-09 17:28:48 +0900

    로그인수정/토큰.

--- backend/user/views.py
+++ backend/user/views.py
@@ -3,7 +3,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from .models import User
 from .serializers import UserSerializer  
 from django.contrib.auth.hashers import make_password
 
