cacd0a7c7d5f39f6c8aacca44219c624a44c1286
Mansik_kun <wjdalstjr730@gmail.com>
2024-07-26 14:49:13 +0900

    Feat:Nav 페이지 생성

--- frontend/src/components/SearchNav.vue
+++ frontend/src/components/SearchNav.vue
@@ -16,13 +16,13 @@
       </div>
 
       <div class="modal-btn" >
-        <button id="nav-button">
+        <button id="nav-button" @click="openNavModal">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-send-fill" viewBox="0 0 16 16">
             <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471z"/>
           </svg>
         </button>
 
-        <div class="modal-nav-wrap" v-show="secondModalOpen" >
+        <div class="modal-nav-wrap" v-show="secondModalOpen" @click="closeNavModals">
           <div class="modal-nav-container" @click="preventClose">
             <p>HI</p>
             
@@ -117,7 +117,7 @@
         this.firstDetailModalOpen = true;
         this.closeModalsExcept('firstDetailModalOpen');
       },
-      openQuikModal() {
+      openNavModal() {
         this.secondModalOpen = true;
         this.closeModalsExcept('secondModalOpen');
       },
@@ -158,7 +158,7 @@
         this.favoriteModalOpen = true;
         this.firstModalOpen = false;
       },
-      closeQuikModals() {
+      closeNavModals() {
         this.favoriteModalOpen = true;
         this.secondModalOpen = false;
       },
