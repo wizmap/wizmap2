25c7ca272bccfdb8db99803415396411e64a300e
Mansik_kun <wjdalstjr730@gmail.com>
2024-07-26 10:07:29 +0900

    Fix:필요없는 함수 제거

--- frontend/src/components/SearchFavorite.vue
+++ frontend/src/components/SearchFavorite.vue
@@ -8,7 +8,7 @@
     <div class="modal-favorite-container" @click="preventClose">
 
       <div class="modal-btn">
-        <router-link to="/searchresult" id="modal-search-button" @click="openSearchModal">
+        <router-link to="/searchresult" id="modal-search-button">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
           </svg>
@@ -16,15 +16,16 @@
       </div>
 
       <div class="modal-btn" >
-        <button id="quickslot-button" @click="openQuickModal">
+        <button id="nav-button">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-send-fill" viewBox="0 0 16 16">
             <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471z"/>
           </svg>
         </button>
-        <div class="modal-quickslot-wrap" v-show="secondModalOpen" @click="closeQuickModals">
-          <div class="modal-quickslot-container" @click="preventClose">
-            
 
+        <div class="modal-nav-wrap" v-show="secondModalOpen" >
+          <div class="modal-nav-container" @click="preventClose">
+            <p>HI</p>
+            
            
           </div>
         </div>
@@ -302,19 +303,6 @@
         this.favoriteModalOpen = true;
         this.closeModalsExcept('favoriteModalOpen');
       },
-      openSearchModal() {
-        this.firstModalOpen = true;
-        this.closeModalsExcept('firstModalOpen');
-      },
-      openSearchDetailModal(result) {
-        this.fetchPlaceDetails(result.place.id)//id값으로 장소데이터 불러오기
-        this.firstDetailModalOpen = true;
-        this.closeModalsExcept('firstDetailModalOpen');
-      },
-      openQuickModal() {
-        this.secondModalOpen = true;
-        this.closeModalsExcept('secondModalOpen');
-      },
       openFavModal() {
         this.thirdModalOpen = true;
         this.closeModalsExcept('thirdModalOpen');
@@ -327,14 +315,6 @@
         this.thirdDetailModalOpen = true;
         this.closeModalsExcept('thirdDetailModalOpen');
       },
-      openHisModal() {
-        this.fourthModalOpen = true;
-        this.closeModalsExcept('fourthModalOpen');
-      },
-      openModal() {
-        this.modalOpen = true;
-        this.closeModalsExcept('modalOpen');
-      },
       closeFavoriteModals() {
         this.favoriteModalOpen = false;
       },
@@ -344,29 +324,9 @@
         this.thirdModalOpen = true;
         this.thirdDetailModalOpen = false;
       },
-      closeSearchDetailModals() {
-        this.favoriteModalOpen = true;
-        this.firstModalOpen = true;
-        this.firstDetailModalOpen = false;
-      },
-      closeSearchModals() {
-        this.favoriteModalOpen = true;
-        this.firstModalOpen = false;
-      },
-      closeQuickModals() {
-        this.favoriteModalOpen = true;
-        this.secondModalOpen = false;
-      },
       closeFavModals() {
         this.favoriteModalOpen = true;
         this.thirdModalOpen = false;
-      },
-      closeHisModals() {
-        this.favoriteModalOpen = true;
-        this.fourthModalOpen = false;
-      },
-      closeModal() {
-        this.modalOpen = false;
       },
       preventClose(event) {
         event.stopPropagation();
